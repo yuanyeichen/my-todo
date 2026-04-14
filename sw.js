adde470023b4d29de7e076ca7e80017698316a64
yuanye <yuanye@192.168.124.30>
2026-04-14 16:34:26 +0800

    feat: 拖拽排序、批量操作、Undo撤销、快捷键、撒花动画、PWA、导入导出

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'todo-app-v1';
+const urlsToCache = ['.', './index.html'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => response || fetch(e.request))
+    );
+});
