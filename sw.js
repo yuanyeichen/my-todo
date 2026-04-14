ddcf3d3645a97e7581c258020c6075678759b3b3
yuanye <yuanye@192.168.124.30>
2026-04-14 19:43:14 +0800

    fix: 强制刷新PWA缓存，添加iOS Safari安装引导条

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
-const CACHE_NAME = 'todo-app-v1';
-const urlsToCache = ['.', './index.html'];
+const CACHE_NAME = 'todo-app-v2';
+const urlsToCache = ['.', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
